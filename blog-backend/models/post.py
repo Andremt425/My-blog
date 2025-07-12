3bf63b1f3ff9623960d295aa3153d73aa4cafc62
Andremt425 <andretoerien425@gmail.com>
2025-07-12 22:45:43 +0200

    added api routes

--- blog-backend/models/post.py
+++ blog-backend/models/post.py
@@ -1,17 +1,13 @@
-class post:
-    def __init__(self, title, content, author, created_at, likes=0, comments=None):
-        self.title = title
-        self.content = content
-        self.author = author
-        self.created_at = created_at
-        self.likes = likes
-        self.comments = comments if comments is not None else []
+from pydantic import BaseModel
 
-    def add_comment(self, comment):
-        self.comments.append(comment)
-    
-    def add_like(self):
-        self.likes += 1
-    
-    def update_content(self, new_content):
-        self.content = new_content
+class Comment(BaseModel):
+    comment : str
+
+class Post(BaseModel):
+    title: str 
+    content: str
+    author: str
+    created_at: str
+    likes: int
+    comments: Comment = []
+
